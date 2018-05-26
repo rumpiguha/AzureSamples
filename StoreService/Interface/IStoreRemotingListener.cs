2616fca2efeca6149efadbd6db3dd6d2e4513292
Rumpi Guha <ruguha@microsoft.com>
2018-05-26 16:30:28 +0530

    Commit for stateless client

---
+++ StoreService/Interface/IStoreRemotingListener.cs
@@ -0,0 +1,23 @@
+﻿using DataStorage.Models;
+using Microsoft.ServiceFabric.Services.Remoting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreService.Interface
+{
+    /// <summary>
+    /// Store Service Interface
+    /// </summary>
+    public interface IStoreRemotingListener : IService
+    {
+        /// <summary>
+        /// Method to get stores from state manager
+        /// </summary>
+        /// <param name="request">The request </param>
+        /// <returns>Asynchronous Task</returns>
+        Task<List<Store>> GetStoresById(string request);
+    }
+}
